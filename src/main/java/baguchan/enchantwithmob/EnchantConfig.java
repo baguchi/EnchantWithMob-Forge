59395201dd465108342b340fa084db4d1e1feace
baguchan <shutux500@gmail.com>
2025-05-05 20:48:51 +0900

    aura render is now default is enable

--- src/main/java/baguchan/enchantwithmob/EnchantConfig.java
+++ src/main/java/baguchan/enchantwithmob/EnchantConfig.java
@@ -38,7 +38,7 @@ public class EnchantConfig {
                     .define("Disable Poison Particle", true);
             disableAuraRender = builder
                     .comment("Disable Aura Render. [true / false]")
-                    .define("Disable Aura Render", true);
+                    .define("Disable Aura Render", false);
             oldStyleAnimation = builder
                     .comment("Enable Old Style Animation. [true / false]")
                     .define("OldStyleAnimation", false);
