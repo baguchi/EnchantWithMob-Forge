9bcc81f114e8a597f4d1db42c4031c7995807463
baguchan <shutux500@gmail.com>
2025-05-29 21:04:49 +0900

    1.20.1-11.17.0

--- src/main/java/baguchan/enchantwithmob/mixin/ServerLevelMixin.java
+++ src/main/java/baguchan/enchantwithmob/mixin/ServerLevelMixin.java
@@ -34,8 +34,8 @@ public abstract class ServerLevelMixin extends Level {
     public void tickNonPassenger(Entity instance, CallbackInfo ci) {
         if (instance instanceof IEnchantCap enchantCap && instance instanceof IEnchantedTime enchantedTime) {
             //ajust time
-            int fastTime = Mth.clamp(MobEnchantUtils.getMobEnchantLevelFromHandler(enchantCap.getEnchantCap().getMobEnchants(), MobEnchants.FAST.get()), 0, 2);
-            int slowTime = Mth.clamp(MobEnchantUtils.getMobEnchantLevelFromHandler(enchantCap.getEnchantCap().getMobEnchants(), MobEnchants.SLOW.get()), 0, 2);
+            int fastTime = Mth.clamp(MobEnchantUtils.getMobEnchantLevelFromHandler(enchantCap.getEnchantCap().getMobEnchants(), MobEnchants.FAST.get()), 0, 10);
+            int slowTime = Mth.clamp(MobEnchantUtils.getMobEnchantLevelFromHandler(enchantCap.getEnchantCap().getMobEnchants(), MobEnchants.SLOW.get()), 0, 10);
 
             float different = 1 + fastTime * 0.125F - slowTime * 0.125F;
 
--- src/main/java/baguchan/enchantwithmob/mixin/client/LivingEntityRendererMixin.java
+++ src/main/java/baguchan/enchantwithmob/mixin/client/LivingEntityRendererMixin.java
@@ -46,8 +46,8 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extend
         if (this.getModel() instanceof IEnchantedTime enchantedTime) {
             if (instance instanceof IEnchantCap enchantCap) {
                 //ajust time
-                int fastTime = Mth.clamp(MobEnchantUtils.getMobEnchantLevelFromHandler(enchantCap.getEnchantCap().getMobEnchants(), MobEnchants.FAST.get()), 0, 2);
-                int slowTime = Mth.clamp(MobEnchantUtils.getMobEnchantLevelFromHandler(enchantCap.getEnchantCap().getMobEnchants(), MobEnchants.SLOW.get()), 0, 2);
+                int fastTime = Mth.clamp(MobEnchantUtils.getMobEnchantLevelFromHandler(enchantCap.getEnchantCap().getMobEnchants(), MobEnchants.FAST.get()), 0, 255);
+                int slowTime = Mth.clamp(MobEnchantUtils.getMobEnchantLevelFromHandler(enchantCap.getEnchantCap().getMobEnchants(), MobEnchants.SLOW.get()), 0, 255);
                 float different = 1 + fastTime * 0.125F - slowTime * 0.125F;
 
                 enchantedTime.setDifferentTime(different);
