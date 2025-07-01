09173ec17c66d38e82facdf8a9638941eb977bc2
baguchan <shutux500@gmail.com>
2025-07-01 17:37:03 +0900

    1.21.7-25.0.1

--- src/main/java/baguchi/enchantwithmob/mobenchant/ProtectionMobEnchant.java
+++ src/main/java/baguchi/enchantwithmob/mobenchant/ProtectionMobEnchant.java
@@ -1,5 +1,11 @@
 package baguchi.enchantwithmob.mobenchant;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import org.apache.commons.lang3.mutable.MutableFloat;
+
 public class ProtectionMobEnchant extends MobEnchant {
     public ProtectionMobEnchant(Properties properties) {
         super(properties);
@@ -11,5 +17,10 @@ public class ProtectionMobEnchant extends MobEnchant {
 
     public int getMaxEnchantability(int enchantmentLevel) {
         return this.getMinEnchantability(enchantmentLevel) + 11;
+    }
+
+    @Override
+    public void modifyDamageProtection(ServerLevel level, int p344605, LivingEntity entity, DamageSource damageSource, MutableFloat mutablefloat) {
+        mutablefloat.setValue(mutablefloat.getValue() + Mth.floor((float) ((6 + p344605) * 1.5F) / 3));
     }
 }
