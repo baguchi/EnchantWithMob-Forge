7f3d48cecacafdc8f35cd203b46691a3d50a8380
baguchan <shutux500@gmail.com>
2026-01-23 06:04:29 +0900

    1.21.11-29.6.0 add disableEnchantedEye config and fixed misspell

--- src/main/java/baguchi/enchantwithmob/client/render/layer/EnchantedEyesLayer.java
+++ src/main/java/baguchi/enchantwithmob/client/render/layer/EnchantedEyesLayer.java
@@ -36,8 +36,8 @@ public class EnchantedEyesLayer<T extends LivingEntityRenderState, M extends Ent
     public void submit(PoseStack p_116983_, SubmitNodeCollector submitNodeCollector, int p_116985_, T entity, float p_116987_, float p_116988_) {
         boolean enchanted = entity.getRenderDataOrDefault(EnchantLayer.ENCHANTED, false);
 
-        if (enchanted && !EnchantConfig.CLIENT.disableAuraRender.get()) {
-            submitNodeCollector.submitModel(this.getParentModel(), entity, p_116983_, this.renderType(), p_116985_, OverlayTexture.NO_OVERLAY, -1,
+		if (enchanted && !EnchantConfig.CLIENT.disableEyeRender.get()) {
+			submitNodeCollector.submitModel(this.getParentModel(), entity, p_116983_, this.renderType(), p_116985_, OverlayTexture.NO_OVERLAY, -1,
                         null,
                         entity.outlineColor,
                         null);
