4a4a74c6ff0ba04fcd5bad09bb7f7e581a03f887
baguchan <shutux500@gmail.com>
2026-02-10 14:31:59 +0900

    customizable eye texture

--- src/main/java/baguchi/enchantwithmob/client/ClientRegistrar.java
+++ src/main/java/baguchi/enchantwithmob/client/ClientRegistrar.java
@@ -2,6 +2,7 @@ package baguchi.enchantwithmob.client;
 
 import baguchi.enchantwithmob.EnchantWithMob;
 import baguchi.enchantwithmob.api.IEnchantCap;
+import baguchi.enchantwithmob.api.MobEnchantEye;
 import baguchi.enchantwithmob.client.model.EnchantedWindModel;
 import baguchi.enchantwithmob.client.model.EnchanterModel;
 import baguchi.enchantwithmob.client.overlay.MobEnchantOverlay;
@@ -10,6 +11,7 @@ import baguchi.enchantwithmob.client.render.layer.EnchantLayer;
 import baguchi.enchantwithmob.client.render.layer.EnchantedEyesLayer;
 import baguchi.enchantwithmob.client.render.layer.EnchantedWindLayer;
 import baguchi.enchantwithmob.client.render.layer.SlimeEnchantLayer;
+import baguchi.enchantwithmob.data.resources.registries.MobEnchantEyes;
 import baguchi.enchantwithmob.registry.MobEnchants;
 import baguchi.enchantwithmob.registry.ModEntities;
 import baguchi.enchantwithmob.utils.MobEnchantUtils;
@@ -24,9 +26,8 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.SlimeRenderer;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
-import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -35,6 +36,8 @@ import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RegisterRenderPipelinesEvent;
 import net.neoforged.neoforge.client.renderstate.RegisterRenderStateModifiersEvent;
+
+import java.util.Optional;
 
 import static net.minecraft.client.renderer.RenderPipelines.*;
 
@@ -53,21 +56,6 @@ public class ClientRegistrar {
 			RenderPipeline.builder(new RenderPipeline.Snippet[]{MATRICES_PROJECTION_SNIPPET, FOG_SNIPPET, GLOBALS_SNIPPET})
                     .withLocation(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "pipeline/mob_enchant_eye"))
 					.withVertexShader("core/glint").withFragmentShader("core/glint").withSampler("Sampler0").withDepthWrite(false).withCull(false).withBlend(new BlendFunction(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA, SourceFactor.ONE, DestFactor.ONE_MINUS_SRC_ALPHA)).withVertexFormat(DefaultVertexFormat.POSITION_TEX, VertexFormat.Mode.QUADS).build();
-
-
-    private static final RenderType BLAZE_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_blaze_eyes.png"));
-    private static final RenderType CREEPER_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_creeper_eyes.png"));
-    private static final RenderType EVOKER_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_evoker_eyes.png"));
-    private static final RenderType PILLAGER_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_pillager_eyes.png"));
-    private static final RenderType SKELETON_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_skeleton_eyes.png"));
-    private static final RenderType SLIME_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_slime_eyes.png"));
-    private static final RenderType SPIDER_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_spider_eyes.png"));
-    private static final RenderType VEX_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_vex_eyes.png"));
-    private static final RenderType VINDICATOR_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_vindicator_eyes.png"));
-    private static final RenderType WITCH_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_witch_eyes.png"));
-    private static final RenderType WOLF_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_wolf_eyes.png"));
-    private static final RenderType ZOMBIE_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_zombie_eyes.png"));
-    private static final RenderType GUARDIAN_EYES = EnchantedEyesLayer.enchantedEyes(Identifier.fromNamespaceAndPath(EnchantWithMob.MODID, "textures/entity/enchant_eye/enchanted_guardian_eyes.png"));
 
 	@SubscribeEvent
 	public static void registerEntityRenders(EntityRenderersEvent.RegisterRenderers event) {
@@ -105,43 +93,7 @@ public class ClientRegistrar {
 
 
 			if (event.getRenderer(entityType) instanceof LivingEntityRenderer r) {
-				if (entityType == EntityType.BLAZE) {
-					r.addLayer(new EnchantedEyesLayer(r, BLAZE_EYES));
-				}
-				if (entityType == EntityType.CREEPER) {
-					r.addLayer(new EnchantedEyesLayer(r, CREEPER_EYES));
-				}
-				if (entityType == EntityType.EVOKER) {
-					r.addLayer(new EnchantedEyesLayer(r, EVOKER_EYES));
-				}
-				if (entityType == EntityType.PILLAGER) {
-					r.addLayer(new EnchantedEyesLayer(r, PILLAGER_EYES));
-				}
-				if (entityType == EntityType.STRAY || entityType == EntityType.WITHER || entityType == EntityType.SKELETON || entityType == EntityType.BOGGED) {
-					r.addLayer(new EnchantedEyesLayer(r, SKELETON_EYES));
-				}
-				if (entityType == EntityType.SLIME) {
-					r.addLayer(new EnchantedEyesLayer(r, SLIME_EYES));
-				}
-				if (entityType == EntityType.SPIDER || entityType == EntityType.CAVE_SPIDER) {
-					r.addLayer(new EnchantedEyesLayer(r, SPIDER_EYES));
-				}
-				if (entityType == EntityType.VINDICATOR) {
-					r.addLayer(new EnchantedEyesLayer(r, VINDICATOR_EYES));
-				}
-				if (entityType == EntityType.WITCH) {
-					r.addLayer(new EnchantedEyesLayer(r, WITCH_EYES));
-				}
-				if (entityType == EntityType.WOLF) {
-					r.addLayer(new EnchantedEyesLayer(r, WOLF_EYES));
-				}
-				if (entityType == EntityType.ZOMBIE || entityType == EntityType.HUSK) {
-					r.addLayer(new EnchantedEyesLayer(r, ZOMBIE_EYES));
-				}
-				if (entityType == EntityType.GUARDIAN || entityType == EntityType.ELDER_GUARDIAN) {
-					r.addLayer(new EnchantedEyesLayer(r, GUARDIAN_EYES));
-				}
-
+				r.addLayer(new EnchantedEyesLayer(r));
 			}
 		});
     }
@@ -163,8 +115,11 @@ public class ClientRegistrar {
 
 				MobEnchantUtils.executeIfPresent(entity, MobEnchants.WIND.getKey(), () -> {
 					state.setRenderData(EnchantedWindLayer.WIND, true);
-				});
 
+				});
+				Optional<Holder.Reference<MobEnchantEye>> enchantEye = MobEnchantEyes.getEyeVariant(entity.registryAccess(), entity.typeHolder());
+
+				enchantEye.ifPresent(mobEnchantEye -> state.setRenderData(EnchantedEyesLayer.ENCHANT_EYE, mobEnchantEye.value().texture()));
 			}
 		});
 	}
