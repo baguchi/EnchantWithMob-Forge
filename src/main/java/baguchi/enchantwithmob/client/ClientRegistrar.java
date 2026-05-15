c395551cd4af54148f29070684a691d15d0d9c84
baguchi <shutux500@gmail.com>
2026-05-15 13:44:56 +0900

    26.1.2-32.5.0 now enchanter beam is correct position

--- src/main/java/baguchi/enchantwithmob/client/ClientRegistrar.java
+++ src/main/java/baguchi/enchantwithmob/client/ClientRegistrar.java
@@ -117,7 +117,7 @@ public class ClientRegistrar {
 				if (cap.getEnchantCap().getEnchantOwner().isPresent()) {
 					LivingEntity ownerEntity = EntityReference.getLivingEntity(cap.getEnchantCap().getEnchantOwner().get(), Minecraft.getInstance().player.level());
 					if (ownerEntity != null) {
-						state.setRenderData(ClientEventHandler.ENCHANTER_POS, ownerEntity.position());
+						state.setRenderData(ClientEventHandler.ENCHANTER_POS, ownerEntity.getEyePosition());
 					}
 				} else {
 					state.setRenderData(ClientEventHandler.ENCHANTER_POS, null);
