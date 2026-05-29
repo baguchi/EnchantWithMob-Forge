838ea8d55bbef3fd302a745a758e35f864584eb6
baguchi <shutux500@gmail.com>
2026-05-30 07:31:14 +0900

    26.1.2-32.9.0 fixed not selfed mob enchant overlay(display another mob's mob enchant)

--- src/main/java/baguchi/enchantwithmob/client/overlay/MobEnchantOverlay.java
+++ src/main/java/baguchi/enchantwithmob/client/overlay/MobEnchantOverlay.java
@@ -41,7 +41,7 @@ public class MobEnchantOverlay implements GuiLayer {
             if (EnchantConfig.CLIENT.showEnchantedMobHud.get() && mc.crosshairPickEntity != null) {
                 if (mc.crosshairPickEntity instanceof IEnchantCap cap) {
                     if (cap.getEnchantCap().hasEnchant()) {
-                        guiGraphics.text(mc.font, mc.crosshairPickEntity.getDisplayName(), (int) 20, (int) 50, -1);
+                        guiGraphics.text(mc.font, mc.crosshairPickEntity.getDisplayName(), (int) EnchantConfig.CLIENT.hudXPostion.getAsInt(), (int) EnchantConfig.CLIENT.hudYPostion.getAsInt(), -1);
 
                         for (MobEnchantHandler mobEnchantHandler : cap.getEnchantCap().getMobEnchants()) {
                             Holder<MobEnchant> mobEnchant = mobEnchantHandler.getMobEnchant();
@@ -51,8 +51,8 @@ public class MobEnchantOverlay implements GuiLayer {
 
                             Component s = mobEnchantHandler.getMobEnchant().value().getFullname(mobEnchantHandler.getEnchantLevel());
 
-                            int xOffset = 20;
-                            int yOffset = cap.getEnchantCap().getMobEnchants().indexOf(mobEnchantHandler) * 10 + 60;
+                            int xOffset = 20 + EnchantConfig.CLIENT.hudXPostion.getAsInt();
+                            int yOffset = cap.getEnchantCap().getMobEnchants().indexOf(mobEnchantHandler) * 10 + 10 + EnchantConfig.CLIENT.hudYPostion.getAsInt();
 
                             guiGraphics.text(mc.font, s, (int) (xOffset), (int) yOffset, -1);
                         }
